e3cb2e1679c1a3ae51f041f61d3d7a70526e2c45
Po-Yu Chuang <ratbert.chuang@gmail.com>
2016-02-21 15:07:08 -0500

    cin-reader.py: parse phone.cin

---
+++ cin-reader.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+def convert_key(keyname, s):
+    ns = ''
+    for c in s:
+        ns += keyname[c]
+    return ns
+
+def main():
+    filename = 'phone.cin'
+
+    keyname = {}
+    with open(filename) as f:
+        STATE_INIT = 0
+        STATE_KEYNAME = 1
+        STATE_CHARDEF = 2
+        state = STATE_INIT
+        for line in f:
+            if line[0] == '#':
+                continue
+            e = line.split()
+            if e[0] == '%keyname':
+                if e[1] == 'begin':
+                    state = STATE_KEYNAME
+            elif e[0] == '%chardef':
+                if e[1] == 'begin':
+                    state = STATE_CHARDEF
+            elif state == STATE_KEYNAME:
+                keyname[e[0]] = e[1]
+            elif state == STATE_CHARDEF:
+                mapping = convert_key(keyname, e[0])
+                print(e[1], mapping)
+
+if __name__ == '__main__':
+    main()
